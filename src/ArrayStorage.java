d5fbb3706c65b9bcbc7ab29091388aeba976a9cc
gangmaster <user.shadow@gmail.com>
2019-02-09 17:01:54 +0000

    Lesson 2 ArrayStorage.java (Update). Added some checks methods (Resume, uuid) and a new method update.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -16,43 +16,79 @@ public class ArrayStorage {
         size = 0;
     }
 
+    void update(Resume resume) {
+        //Check for "resume". "Resume" exist? If "true" - update "resume".
+        // If "false" - show error msg.
+        String result = checkResume(resume);
+        if (result != null) {
+            int i = Integer.parseInt(result);
+            storage[i] = resume;
+        } else {
+            System.out.println("ERROR: " + resume + " don't exist.");
+        }
+    }
+
     void save(Resume resume) {
-        //Add "resume" to array. Increment size.
-        storage[size] = resume;
-        size++;
+        //Check for "resume" in array. "Resume" exist? If "true" - print a msg.
+        // If "false" - add "resume" to array. Increment size.
+        String result = checkResume(resume);
+        if (result == null) {
+            storage[size] = resume;
+            size++;
+        } else {
+            System.out.println("ERROR: " + resume + " already exist.");
+        }
     }
 
     Resume get(String uuid) {
-        //Search inside array for "uuid". If "uuid" is found, return "uuid" as result value. In case when "uuid" is not found, catch exception and show "error" msg.
-        Resume result = null;
-        try {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].uuid.equals(uuid)) {
-                    result = storage[i];
-                }
+        //Search inside array for "uuid". If "uuid" is found, return "uuid" as result value.
+        //In case when "uuid" is not found, show error msg.
+        Resume check = checkUuid(uuid);
+        if (check == null) {
+            System.out.println("ERROR: " + uuid + " don't exist.");
+        }
+        return check;
+    }
+
+    void delete(String uuid) {
+        //Check for "uuid" in array. "uuid" found? If "true" - Swap existing "uuids" to "null". After "sort" array to move "null" value to the end.
+        //If "false" - show error msg.
+        Resume result = checkUuid(uuid);
+        if (result != null) {
+            result = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("ERROR: " + uuid + " don't exist.");
+        }
+    }
+
+    String checkResume(Resume resume) {
+        //Check for "Resume" in array. "Resume" found? If "true" - return result array index.
+        //If "false" - show error msg. Return null;
+        String result = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i] == resume) {
+                result = Integer.toString(i);
+                break;
             }
-        } catch (NullPointerException exception) {
-            System.out.println(uuid + " don't exist.");
         }
         return result;
     }
 
-    void delete(String uuid) {
-        //Swap existing "uuids" to "null". After "sort" array to move "null" value to the end.
-        boolean uuidExist = false;
+    Resume checkUuid(String uuid) {
+        //Check for "uuid" in array. "uuid" found? If "true" - return result as array value.
+        //If "false" - show error msg. Return null;
+        Resume result = null;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                uuidExist = true;
-                storage[i] = storage[size-1];
-                storage[size-1] = null;
-                size--;
+                result = storage[i];
+                break;
             }
         }
-
-        if (!uuidExist) {
-            System.out.println(uuid + " don't exist.");
-        }
+        return result;
     }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
