d13722bf7707c14adb25ef0c4987dad44e30ac19
gangmaster <user.shadow@gmail.com>
2019-02-18 22:05:26 +0000

    Added functionality to SortedArrayStorage class methods and some updates in MainArray and mainTestArrayStorage classes.

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -2,9 +2,10 @@
  * Test for your ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    private static final Storage ARRAY_STORAGE = new ArrayStorage();
+    private static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
+        Resume r;
         final  Resume r1 = new Resume();
         r1.setUuid("uuid1");
         final    Resume r2 = new Resume();
@@ -12,9 +13,9 @@ public class MainTestArrayStorage {
         final   Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
+        ARRAY_STORAGE.save(r3);
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
-        ARRAY_STORAGE.save(r3);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
@@ -22,7 +23,9 @@ public class MainTestArrayStorage {
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(r1.getUuid());
+        ARRAY_STORAGE.update(r1);
+        printAll();
+        ARRAY_STORAGE.delete(r2.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
--- src/SortedArrayStorage.java
+++ src/SortedArrayStorage.java
@@ -4,33 +4,73 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
     public void update(Resume resume) {
-
+        int index = findIndex(resume.getUuid());
+        if (index < 0) {
+            System.out.println("ERROR: " + resume.getUuid() + " don't exist.");
+        } else {
+            storage[index] = resume;
+        }
     }
 
     @Override
     public void save(Resume resume) {
-
+        if (size < STORAGE_LIMIT) {
+            int index = findIndex(resume.getUuid());
+            if (index < 0) {
+                storage[size] = resume;
+                size++;
+            } else {
+                System.out.println("ERROR: " + resume + " already exist.");
+            }
+        } else {
+            System.out.println("No empty space in storage!");
+        }
+        sort(storage);
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index = findIndex(uuid);
+        if (index >= 0) {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        } else {
+            System.out.println("ERROR: " + uuid + " don't exist.");
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
+    }
+
+    private void sort(Resume[] array) {
+        boolean isSorted;
+        do {
+            isSorted = false;
+            for (int i = 0; i < size-1; i++) {
+                if (array[i].compareTo(array[i + 1]) > 0) {
+                    Resume temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                    isSorted = true;
+                }
+            }
+        } while ((isSorted));
     }
 
     @Override
     protected int findIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
+        System.out.println(Arrays.binarySearch(storage, 0, size, searchKey));
         return Arrays.binarySearch(storage, 0, size, searchKey);
     }
 
