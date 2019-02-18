0cdeed3b954b5f5b87141cc91407ebfca100401e
gangmaster <user.shadow@gmail.com>
2019-02-18 16:27:58 +0000

    Fix equals

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -57,7 +57,7 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     protected int findIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
